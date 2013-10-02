f48ddebace4913cab653e53a877ee6d4dff1a8ca
Tolik Shevchenko <anatolii.shevchenko@p-product.com>
2013-10-03 00:49:44 +0300

    Device iP getting fix.

--- Features/Features/NetworkInfo/NetworkInfo.m
+++ Features/Features/NetworkInfo/NetworkInfo.m
@@ -102,7 +102,7 @@
                 // Check if interface is en0 which is the wifi connection on the iPhone
                 if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"])
                 {
-                    deviceIP = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_dstaddr)->sin_addr)];
+                    deviceIP = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
                 }
             }
             temp_addr = temp_addr->ifa_next;
